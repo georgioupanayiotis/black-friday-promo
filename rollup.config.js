f8eb89e1641a45b095674df47eedb4446b10061c
Panayiotis Georgiou <panayiotis.georgiou@netmarltd.com>
2025-11-27 16:58:58 +0200

    Optimize deployment with minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
 
 export default {
     input: 'src/index.ts',
@@ -11,9 +12,22 @@ export default {
             sourcemap: true
         },
         {
+            file: 'dist/black-friday-promo.min.js',
+            format: 'umd',
+            name: 'BlackFridayPromo',
+            sourcemap: true,
+            plugins: [terser()]
+        },
+        {
             file: 'dist/black-friday-promo.esm.js',
             format: 'es',
             sourcemap: true
+        },
+        {
+            file: 'dist/black-friday-promo.esm.min.js',
+            format: 'es',
+            sourcemap: true,
+            plugins: [terser()]
         }
     ],
     plugins: [
